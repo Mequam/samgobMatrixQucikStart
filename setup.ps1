f9bfd548a66abaea469816231c63463a6a585480
mequam <blue9ja@gmail.com>
2025-01-02 10:51:27 -0500

    verbose and unit tests :D

--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,15 @@
+echo "begining download..."
 git clone https://github.com/Mequam/samgobmatrix.git
 cd samgobmatrix
 git submodule update --init --recursive
+
+echo "setting up virtual enviornment..."
 python -m venv .venv
 Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
 ./.venv\\Scripts\\activate
+
+echo "installing pythonic dependencies..."
 python -m pip install -r ./requirements.txt
+
+echo "finished install, performing unit tests..."
+python -m unittest -v
--- setup.sh
+++ setup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+echo "[*] begining code download..."
 git clone https://github.com/Mequam/samgobmatrix.git
 cd samgobmatrix
 git submodule update --init --recursive
+
+echo "[*] starting virtual environment..."
 python -m venv .venv
 source .venv/bin/activate
+
+echo "[*] intalling pythonic dependencies..."
 python -m pip install -r ./requirements.txt
+
+echo "[*] finished, performing unit tests..."
+python -m unittest -v
