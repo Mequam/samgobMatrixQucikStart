fb750c5e420350ef16133574d65acfb2da91e9ce
mequam <blue9ja@gmail.com>
2025-01-02 10:37:29 -0500

    removed debug ls

--- setup.ps1
+++ setup.ps1
@@ -3,6 +3,5 @@ cd samgobmatrix
 git submodule update --init --recursive
 python -m venv .venv
 Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
-ls
 ./.venv\\Scripts\\activate
 python -m pip install -r ./requirements.txt
